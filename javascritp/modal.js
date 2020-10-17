2f31a2350dad991abed2bff98e9d854b244be5a4
percycordova <cordova_inf@hotmail.com>
2020-10-17 01:20:32 -0500

    Añadiendo nuevos htmls
    Se desarrollo las paginas de galeria y nosotros
    
    Co-Authored-By: root <39401+root@users.noreply.github.com>

--- javascritp/modal.js
+++ javascritp/modal.js
@@ -1,15 +1,15 @@
-let imagenes=document.querySelectorAll('.galery-img');
-let modal=document.querySelector('#modal');
-let img=document.querySelector('#modal-img');
-let boton=document.querySelector('#modal-boton');
+let imagenes = document.querySelectorAll('.galery-img');
+let modal = document.querySelector('#modal1');
+let img = document.querySelector('#modal1-img');
+let boton = document.querySelector('#modal1-boton');
 
-for(let i=0;i<imagenes.length;i++){
-    imagenes[i].addEventListener('click',function(e){
+for (let i = 0; i < imagenes.length; i++) {
+    imagenes[i].addEventListener('click', function(e) {
         modal.classList.toggle("modal-open");
         let src = e.target.src;
-        img.setAttribute("src",src);
+        img.setAttribute("src", src);
     });
 }
-boton.addEventListener('click',function(){
-modal.classList.toggle("modal-open");
+boton.addEventListener('click', function() {
+    modal.classList.toggle("modal-open");
 });
